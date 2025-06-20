1a1a723b27f0e563ab1220c7f39ef04264558fe4
Fernando <fernandoventurini9@hotmail.com>
2025-06-20 07:23:15 +0100

    Fix Netlify deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath } from "url"; // Método moderno para substituir __dirname
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   build: {
